fe098d20678f7983025bcb8540f227509ee705d3
SERIM KIM <serimkim@SERIMs-MacBook-Pro.local>
2018-10-09 19:06:14 +0900

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
+import { Layout } from 'antd';
 import './App.css';
 
+const { Header, Content, Footer, Sider } = Layout;
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Layout>
+        <Header>header</Header>
+        <Layout>
+          <Sider theme="light">left sidebar</Sider>
+          <Content theme="lgith">main content</Content>
+          <Sider theme="light">right sidebar</Sider>
+        </Layout>
+        <Footer>footer</Footer>
+      </Layout>
     );
   }
 }
