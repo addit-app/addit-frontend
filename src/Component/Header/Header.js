00acbfd652fcf095b8613d32389a6aa39827a936
syaring <sarah.k.serim@gmail.com>
2018-10-26 00:06:03 +0900

    Add login form

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -7,7 +7,7 @@ import {
   Tooltip,
 } from 'antd';
 
-import logo from '../../data/img/logo.png'
+import logo from '../../data/img/icon.png'
 
 const { Header } = Layout;
 const Search = Input.Search;
@@ -16,33 +16,37 @@ class HeaderWrapper extends Component {
   render() {
     return (
       <Header>
-        <Row gutter={16}>
-          <Col span={4}>
-            <div>
-              <img alt="logo" className="logo" src={logo} />
-            </div>
-          </Col>
-          <Col span={10}>
-            <Tooltip
-              placement='right'
-              title='Work in Progress...'
-            >
-              <Search
-                placeholder="input search text"
-                onSearch={value => console.log(value)}
-                disabled
-              />
-            </Tooltip>
-          </Col>
-          <Col
-            span={5}
-            offset={5}
+        <Col
+          span={5}
+        >
+          <div className="logo">
+            <img alt="icon" src={logo} />
+            Addit
+          </div>
+        </Col>
+        <Col
+          span={8}
+          offset={2}
+        >
+          <Tooltip
+            placement='right'
+            title='Work in Progress...'
           >
-            <div className="login">
-              Login | SignUp
-            </div>
-          </Col>
-        </Row>
+            <Search
+              placeholder="input search text"
+              onSearch={value => console.log(value)}
+              disabled
+            />
+          </Tooltip>
+        </Col>
+        <Col
+          span={5}
+          offset={4}
+        >
+          <div className="login">
+            Login | SignUp
+          </div>
+        </Col>
       </Header>
     );
   }
